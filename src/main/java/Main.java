acc20f124ffe3d6b98aea9c59f28c16ba90eb043
maximasemax <78305446+maximasemax@users.noreply.github.com>
2022-04-04 20:44:46 +0400

    snake
    1.0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,45 +11,45 @@ public class Main {
 
 
     public static void main(String[] args) {
-        ComputerCreator computerCreator = new ComputerCreator();
-        Computer computerHiperpc = computerCreator.createHiperpc();
-        Computer computerMediumPc = computerCreator.createMediumPc();
-        Computer computerOfficePc = computerCreator.createOfficePc();
-        Computer computerMediumPc1 = computerCreator.createMediumPc1();
-        System.out.println("Rams with name hiperx");
-        for (Ram ram : computerMediumPc.getRamConfiguration().searchByName("hyperx")){
-            System.out.println(ram);
-        }
-        System.out.println("=============================================================");
-        System.out.println("All Rams Computer");
-        for (Ram ram : computerMediumPc.getRamConfiguration().getAllItems()){
-            System.out.println(ram);
-        }
-        System.out.println("=============================================================");
-        System.out.println("Hdds with name baracuda");
-        for (Hdd hdd : computerMediumPc.getHddConfiguration().searchByName("baracuda")){
-            System.out.println(hdd);
-        }
-        System.out.println("=============================================================");
-        System.out.println("All Hdds Computer");
-        for (Hdd hdd : computerMediumPc.getHddConfiguration().getAllItems()){
-            System.out.println(hdd);
-        }
-        System.out.println("=============================================================");
+//        ComputerCreator computerCreator = new ComputerCreator();
+//        Computer computerHiperpc = computerCreator.createHiperpc();
+//        Computer computerMediumPc = computerCreator.createMediumPc();
+//        Computer computerOfficePc = computerCreator.createOfficePc();
+//        Computer computerMediumPc1 = computerCreator.createMediumPc1();
+//        System.out.println("Rams with name hiperx");
+//        for (Ram ram : computerMediumPc.getRamConfiguration().searchByName("hyperx")){
+//            System.out.println(ram);
+//        }
+//        System.out.println("=============================================================");
+//        System.out.println("All Rams Computer");
+//        for (Ram ram : computerMediumPc.getRamConfiguration().getAllItems()){
+//            System.out.println(ram);
+//        }
+//        System.out.println("=============================================================");
+//        System.out.println("Hdds with name baracuda");
+//        for (Hdd hdd : computerMediumPc.getHddConfiguration().searchByName("baracuda")){
+//            System.out.println(hdd);
+//        }
+//        System.out.println("=============================================================");
+//        System.out.println("All Hdds Computer");
+//        for (Hdd hdd : computerMediumPc.getHddConfiguration().getAllItems()){
+//            System.out.println(hdd);
+//        }
+//        System.out.println("=============================================================");
+//
+//        System.out.println(computerMediumPc.equals(computerMediumPc1));
+//
+//        System.out.println("=============================================================");
+//        Ram newRam = new Ram("gg", 21, 121);
+//        if (!computerHiperpc.getRamConfiguration().editRam("gg", 121,newRam )){
+//            System.out.println("все х ");
+//        }
+//        computerHiperpc.getRamConfiguration().addRam(newRam);
+//        if (!computerHiperpc.getRamConfiguration().editRam("gg", 121,newRam )){
+//            System.out.println("все х ");
+//        }
 
-        System.out.println(computerMediumPc.equals(computerMediumPc1));
-
-        System.out.println("=============================================================");
-        Ram newRam = new Ram("gg", 21, 121);
-        if (!computerHiperpc.getRamConfiguration().editRam("gg", 121,newRam )){
-            System.out.println("все х ");
-        }
-        computerHiperpc.getRamConfiguration().addRam(newRam);
-        if (!computerHiperpc.getRamConfiguration().editRam("gg", 121,newRam )){
-            System.out.println("все х ");
-        }
-
-        //System.out.println(new HaveFan().vivod());
+        System.out.println(new HaveFan().snake());
 
     }
 
--- src/main/java/service/HaveFan.java
+++ src/main/java/service/HaveFan.java
@@ -1,8 +1,115 @@
 package service;
 
+import java.util.Scanner;
+
 public class HaveFan {
     private String zvezda;
     private String tochka;
+
+    public String snake() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите длину одной части змейки по горизонтали:");
+        int horizontalLength = sc.nextInt();
+        System.out.println("Введите длину одной вертикальной части по вертикали:");
+        int verticalLength = sc.nextInt();
+
+        int horizontalCount = 0;
+        int count = 0;
+        while ((horizontalCount + horizontalLength) <= 80){
+            horizontalCount = horizontalCount + horizontalLength;
+            count ++;
+
+        }
+        System.out.println("количество горизонтальных частей , чтоб легче проверить" );
+        System.out.println(count);
+        int countDetour = count;
+        if ((count % 2) != 0) {
+            countDetour = countDetour - 1;
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+
+        for (int x = 0; x == 0; x++) {
+            int horizontalLengthSpace1 = horizontalLength - 1;
+            while (horizontalLengthSpace1 != 0) {
+                stringBuilder.append(" ");
+                horizontalLengthSpace1 = horizontalLengthSpace1 - 1;
+            }
+
+            if ((countDetour % 2) == 0) {
+                int countCounter = countDetour;
+
+                while (countCounter != 0) {
+                    countCounter = countCounter - 2;
+
+                    int horizontalLengthCount = horizontalLength;
+                    while (horizontalLengthCount != 0) {
+                        stringBuilder.append("*");
+                        horizontalLengthCount = horizontalLengthCount - 1;
+                    }
+
+                    int horizontalLengthSpace2 = horizontalLength - 2;
+                    while (horizontalLengthSpace2 != 0) {
+                        stringBuilder.append(" ");
+                        horizontalLengthSpace2 = horizontalLengthSpace2 - 1;
+                    }
+                }
+                stringBuilder.append("\n");
+            }
+        }
+
+
+        int verticalLength1 = verticalLength;
+
+        for (int x = 1; x <= verticalLength1; x++) {
+            stringBuilder.append(" ");
+            int countCounterVertical = countDetour;
+
+            while (countCounterVertical != 0) {
+                countCounterVertical = countCounterVertical - 1;
+
+                int horizontalLengthSpace2 = horizontalLength - 2;
+                while (horizontalLengthSpace2 != 0) {
+                    stringBuilder.append(" ");
+                    horizontalLengthSpace2 = horizontalLengthSpace2 - 1;
+                }
+                stringBuilder.append("*");
+
+            }
+            stringBuilder.append("\n");
+        }
+
+
+        for (int x = 0; x == 0; x++) {
+            if ((countDetour % 2) == 0) {
+                int countCounter1 = countDetour;
+                while (countCounter1 != 0) {
+                    countCounter1 = countCounter1 - 2;
+                    int horizontalLength2 = horizontalLength;
+                    while (horizontalLength2 != 0) {
+                        stringBuilder.append("*");
+                        horizontalLength2 = horizontalLength2 - 1;
+                    }
+
+                    int horizontalLengthSpace2 = horizontalLength - 2;
+                    while (horizontalLengthSpace2 != 0) {
+                        stringBuilder.append(" ");
+                        horizontalLengthSpace2 = horizontalLengthSpace2 - 1;
+                    }
+                }
+            }
+        }
+
+        if ((count % 2) != 0) {
+            int horizontalLength2 = horizontalLength;
+            while (horizontalLength2 != 0) {
+                stringBuilder.append("*");
+                horizontalLength2 = horizontalLength2 - 1;
+            }
+        }
+        return stringBuilder.toString();
+    }
 
 
 }
